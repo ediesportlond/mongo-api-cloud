19c07d55e9716639882cb14323b99716232d4d47
Eddie Espinosa <espinosa.edilson@gmail.com>
2022-10-20 16:32:43 -0400

    added update furniture function and route.

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,18 @@
 import functions from "firebase-functions";
 import express from "express";
 import cors from "cors";
-import { getAllFurniture } from "./src/furniture.js";
+import { getAllFurniture, addNewFurniture, updateFurniture } from "./src/furniture.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.get("/", (req,res) =>{
-    res.send("<marquee style='font-size:48pt;color:blue'>Coming soon...</marquee>");
+    res.send("<marquee style='font-size:48pt;color:white; background-color: black'>Coming soon...</marquee>");
 })
 app.get("/furniture", getAllFurniture);
+app.post("/furniture/addnew", addNewFurniture);
+app.post("/furniture/update", updateFurniture);
 
 
 export const api = functions.https.onRequest(app);
--- functions/src/furniture.js
+++ functions/src/furniture.js
@@ -1,16 +1,55 @@
+import { ObjectId } from "mongodb";
 import dbConnect from "./dbConnect.js";
 
-export async function getAllFurniture(req, res){
+export async function getAllFurniture(req, res) {
     const db = dbConnect();
     const furnitureCollection = db.collection('furniture');
 
     const result = await furnitureCollection.find({}).toArray()
-    .catch( err =>{
-        res.status(500).send({success: false, message: err});
-        return;
-    })
+        .catch(err => {
+            res.status(500).send({ success: false, message: err });
+            return;
+        })
     let message = "<marquee style='background-color: black; font-size:48pt;color:green'>";
     result.forEach(doc => message += `${doc.brand} ${doc.model} ${doc.type} ${doc.price} 😱 `);
     message += "</marquee>";
     res.status(201).send(message);
+}
+
+export async function addNewFurniture(req, res) {
+    const db = dbConnect();
+    const furnitureCollection = db.collection('furniture');
+
+    const { brand, model, type, price } = req.body;
+    const doc = { brand, model, type, price: Number(price) }
+
+    doc.createdDate = new Date();
+
+    const result = await furnitureCollection.insertOne(doc)
+        .catch(err => {
+            res.status(500).send({ success: false, message: err })
+            return;
+        })
+
+    res.status(201).send({ success: true, message: result });
+}
+
+export async function updateFurniture(req,res){
+    const db = dbConnect();
+    const furnitureCollection = db.collection('furniture');
+
+    const query = req.body;
+    const id = {_id: new ObjectId(query.id)};
+    delete query.id;
+    const Query = {
+        $set: query
+    }
+
+    const result = await furnitureCollection.findOneAndUpdate(id, Query)
+    .catch(err => {
+        res.status(500).send({success:false, message:err});
+        return;
+    })
+
+    res.status(201).send({success:true, message: result});
 }
